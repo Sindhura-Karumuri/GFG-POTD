05b4c90ab0b5eb890e1136fbea6b222b2914d3af
Mohana Sri Lakshmi Sindhura Karumuri <karu07sind@gmail.com>
2025-01-29 18:36:20 +0530

    Jan29 2025 POTD.java

---
+++ Jan29 2025 POTD.java
@@ -0,0 +1,11 @@
+Implement Pow
+Difficulty: MediumAccuracy: 52.79%Submissions: 13K+Points: 4
+Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+
+class Solution {
+    double power(double b, int e) {
+        // code here
+        double power = Math.pow(b,e);
+        return power;
+    }
+}
